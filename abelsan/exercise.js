34fa02fd887b1b7f0db32465c3e9601f74e19b76
abelsan <abel@mit.edu>
2017-01-19 08:08:08 -0500

    one exercise should pass

---
+++ abelsan/exercise.js
@@ -0,0 +1,24 @@
+var exercise = {};
+
+exercise.first = function() {
+    //-------------------
+    //---- Your Code ----
+    //-------------------    
+    return 'Success: 1st function works!';
+};
+
+exercise.second = function(greeting) {
+    //-------------------
+    //---- Your Code ----
+    //-------------------
+    return 'Error: 2nd function not implemented';
+};
+
+exercise.third = function(user) {
+    //-------------------
+    //---- Your Code ----
+    //------------------- 
+    return 'Error: 3rd function not implemented';
+};
+
+module.exports = exercise;
